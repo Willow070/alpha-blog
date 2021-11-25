7b3571fe198fbf160b6ad2c01f4ce3c9b0341387
Maurice van de Klundert <maurice@nettl-rijswijk.nl>
2021-11-25 17:28:03 +0100

    Add Sign up func

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_25_144657) do
+ActiveRecord::Schema.define(version: 2021_11_25_154842) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2021_11_25_144657) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
