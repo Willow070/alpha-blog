cba93266811840ef11fa23368fcc84c9439997a6
Maurice van de Klundert <maurice@nettl-rijswijk.nl>
2021-12-02 18:23:46 +0100

    Naar huis

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def new
     @user = User.new
@@ -24,7 +26,7 @@ class UsersController < ApplicationController
 
   def update
     if @user.update(user_params)
-      flash["notice"] = "Your account information was successfully updated"
+      flash[:notice] = "Your account information was successfully updated"
       redirect_to user_path
     else
       render 'edit'
@@ -35,7 +37,21 @@ class UsersController < ApplicationController
     @articles = @user.articles.paginate(page: params[:page], per_page: 3)
   end
 
+  def destroy
+    @user.destroy
+    session[:user_id] = nil if !current_user.admin
+    flash[:notice] = "Account and all associated articles successfully deleted"
+    redirect_to articles_path
+  end
+
   private
+
+  def require_same_user
+    if current_user != @user && !current_user.admin
+      flash[:alert] = "You can only edit or delete your own article"
+      redirect_to root_path
+    end
+  end
 
   def user_params
       params.require(:user).permit(:username, :email, :password)
