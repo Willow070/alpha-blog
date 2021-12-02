83ec53b08f065bf95c3b890e94e9c5bc82bb9225
Maurice van de Klundert <maurice@nettl-rijswijk.nl>
2021-12-02 16:42:58 +0100

    Save

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,18 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+
   def new
     @user = User.new
   end
 
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page], per_page: 3)
   end
 
   def create
-    @user = User.new(user_params)
     if @user.save
         flash[:notice] = "Welcome #{@user.username} to the Blog!"
+        session[:user_id] = @user.id
         redirect_to articles_path
     else
       render 'new'
@@ -18,11 +20,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-      @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash["notice"] = "Your account information was successfully updated"
       redirect_to user_path
@@ -32,13 +32,16 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
-    @articles = @user.articles
+    @articles = @user.articles.paginate(page: params[:page], per_page: 3)
   end
 
   private
 
   def user_params
       params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
